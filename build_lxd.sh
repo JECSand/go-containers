c8197a91e01307e7be1eabdcf987359b79b68472
jecsand <jecsand@pm.me>
2021-01-14 23:57:30 -0600

    updated config

--- build_lxd.sh
+++ build_lxd.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sudo apt-get -y update && sudo apt-get -y upgrade
-sudo snap install core
-sudo snap install lxd
-sudo systemctl stop lxd
-sudo systemctl stop lxd.socket
-sudo systemctl start lxd.socket
 
     cat <<EOF | lxd init --preseed
 profiles:
